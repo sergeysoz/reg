3e3f15eb9975cbedd7717be2fca8a77c3b2686e5
Sergey Sozontov <106061320+sergeysoz@users.noreply.github.com>
2023-01-16 22:54:55 +0500

    CreateKey

--- ReadStringFromRegistry/RegistryCreateKey.cpp
+++ ReadStringFromRegistry/RegistryCreateKey.cpp
@@ -1,14 +1,25 @@
-#include "RegistryCreateKey.h"
+#include ".\RegistryCreateKey.h"
+#include <Windows.h>                // Win32 SDK
+#include <atldef.h>                 // Some ATL stuff
 
 namespace win32
 {
-	LSTATUS RegistryCreateKey(
+	typedef DWORD SECURITY_INFORMATION, * PSECURITY_INFORMATION;
+
+	int RegistryCreateKey(
 		_In_		HKEY	hKey,
 		_In_opt_	LPCSTR	lpSubKey,
 		_Out_		PHKEY	phkResult
 	)
 	{
+		LONG result = ::RegGetKeySecurity(
+			hKey,
+			ATTRIBUTE_SECURITY_INFORMATION,
+			nullptr,
+			nullptr
+		);
 
-		//
+		if (result != 0) return 1;
+		return 0;
 	}
 }
--- ReadStringFromRegistry/RegistryCreateKey.h
+++ ReadStringFromRegistry/RegistryCreateKey.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <Windows.h> 
 
-typedef LONG LSTATUS;
+//typedef LONG LSTATUS;
 
 namespace win32
 {
-	LSTATUS RegistryCreateKey(
+	int RegistryCreateKey(
 		_In_		HKEY	hKey,
 		_In_opt_	LPCSTR	lpSubKey,
 		_Out_		PHKEY	phkResult
